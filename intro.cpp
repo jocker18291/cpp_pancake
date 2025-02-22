8a705305fae71cc0a32de4b2d87dc51aaa10513d
unknown <dawidans714@gmail.com>
2025-02-22 21:03:48 +0100

    Test

---
+++ intro.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float num1 = 0, num2 = 0;
+    char operation;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    cout << "Enter your operation (+,-,*,/): ";
+    cin >> operation;
+    switch(operation){
+        case '+':
+            cout << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << num1 * num2 << endl;
+            break;
+        case '/':
+            if(num2 == 0){
+                cout << "Error: cannot divide by zero." << endl;
+            }
+            else{
+                cout << num1 / num2 << endl;
+            }
+            break;
+        default:
+            cout << "Error: Unknown operation. Try next time." << endl;
+            break;
+    }
+    return 0;
+}
