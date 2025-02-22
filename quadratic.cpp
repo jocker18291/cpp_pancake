f7f6a0e5e756a7a48585dba4316e2b4dc031a8b4
unknown <dawidans714@gmail.com>
2025-02-22 21:30:46 +0100

    Simple bug fixed

--- quadratic.cpp
+++ quadratic.cpp
@@ -7,7 +7,7 @@ int main() {
     float a = 0, b = 0, c = 0;
     cout << "Enter your coefficients: ";
     cin >> a >> b >> c;
-    float delta = (b * b) + 4 * a * c;
+    float delta = (b * b) - 4 * a * c;
     if (delta < 0) {
         cout << "No real solutions." << endl;
     }
