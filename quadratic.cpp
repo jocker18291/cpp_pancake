675d251c36dc2c5058e24131b1692814d2706d2d
jocker18291 <dawidans714@gmail.com>
2025-02-23 14:20:56 +0100

    Add user-input precision

--- quadratic.cpp
+++ quadratic.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
     float a = 0, b = 0, c = 0;
+    int p = 0;
     cout << "Enter your coefficients: ";
     cin >> a >> b >> c;
     float delta = (b * b) - 4 * a * c;
+    cout << "Set precision of your solutions: ";
+    cin >> p;
     if(a != 0){
         if (delta < 0) {
             cout << "No real solutions." << endl;
         }
         else if(delta == 0) {
-            cout << "The solution equals to: " << (-1 * b) / (2 * a) << endl;
+            cout << "The solution equals to: " << setprecision(p) << (-1 * b) / (2 * a) << endl;
         }
         else {
-            cout << "Solutions are equal to " << ((-1 * b) + sqrt(delta)) / (2 * a) << " and " << ((-1 * b) - sqrt(delta)) / (2 * a) << endl;
+            cout << "Solutions are equal to " << setprecision(p) << ((-1 * b) + sqrt(delta)) / (2 * a) << " and " << setprecision(p) << ((-1 * b) - sqrt(delta)) / (2 * a) << endl;
         }
     }
     else if(a == 0) {
         if(b != 0){
-            cout << "It's linear and the solution is: " << (-1 * c) / b << endl;
+            cout << "It's linear and the solution is: " << setprecision(p) << (-1 * c) / b << endl;
         }
         else if(b == 0 && c != 0){
             cout << "The equation has no solutions." << endl;
