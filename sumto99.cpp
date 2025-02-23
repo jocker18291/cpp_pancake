f54a9763e706e291cf6f77aa76bc41e64877ef71
jocker18291 <dawidans714@gmail.com>
2025-02-23 14:54:06 +0100

    First prototype stage

--- sumto99.cpp
+++ sumto99.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 
 using namespace std;
+
+int sum(){
+    int n = 0;
+    int S = 0;
+    while (n != 99){
+        cout << "Enter numbers to sum (only summing nums from -15 to 15; 99 will end the loop): ";
+        cin >> n;
+        if (n <= -15 || n >= 15){
+            S += n;
+        }
+    }
+    return S;
+}
+
+int main(){
+    int solution = sum();
+    cout << "The sum equals: " << solution << endl;
+    return 0;
+}
