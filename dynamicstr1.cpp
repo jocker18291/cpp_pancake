1cb9b955771427c44dcdfef0fb778c7e2402c7ce
jocker18291 <dawidans714@gmail.com>
2025-02-23 23:14:04 +0100

    Add simple function like setArr, getArr and numOfElem

--- dynamicstr1.cpp
+++ dynamicstr1.cpp
@@ -2,7 +2,34 @@
 
 using namespace std;
 
-int main() {
+const int SIZE = 15;
 
+void setArr(char str[]) {
+    cout << "Enter a string of chars [max 15 characters]: ";
+    cin.getline(str, SIZE);
+}
+
+void getArr(char str[]) {
+    for (int i = 0; i < SIZE; i++) {
+        cout << str[i];
+    }
+    cout << endl;
+}
+
+int numOfElem(char str[]) {
+    int sum = 0;
+    for (int i = 0; i < SIZE; i++) {
+        if(str[i] != '\0') {
+            sum++;
+        }
+    }
+    return sum;
+}
+
+int main() {
+    char str[SIZE];
+    setArr(str);
+    getArr(str);
+    cout << numOfElem(str) << endl;
     return 0;
 }
