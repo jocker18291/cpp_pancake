ceef41eea3e75035ec29fecf71e1a38cdb848ce7
jocker18291 <dawidans714@gmail.com>
2025-02-23 19:14:44 +0100

    Sum of elements added

--- array.cpp
+++ array.cpp
@@ -4,26 +4,38 @@ using namespace std;
 
 const int SIZE = 5;
 
-void setElem() {
-    int arr[SIZE];
+void setElem(int arr[]) {
     for(int i = 0; i < SIZE; i++) {
         cout << "Enter the element no. " << i+1 << " : ";
         cin >> arr[i];
     }
 }
 
+void sumOfElem(int arr[]) {
+    int S = 0;
+    for (int i = 0; i < SIZE; i++) {
+        S += arr[i];
+    }
+    cout << S << endl;
+}
+
 int main() {
+    int arr[SIZE];
     int choice = 0;
     do {
         cout << "0 - Exit" << endl;
-        cout << "1 - Set elements in an array of size 5" << endl;
+        cout << "1 - Set elements in the array of size " << SIZE << endl;
+        cout << "2 - Sum of all elements of the array" << endl;
         cout << "Choose: ";
         cin >> choice;
         switch(choice) {
             case 0:
                 break;
             case 1:
-                setElem();
+                setElem(&arr[SIZE]);
+                break;
+            case 2:
+                sumOfElem(&arr[SIZE]);
                 break;
             default:
                 cout << "Choice unknown. Try again." << endl;
