abe13ed0908e1fc4b828b1ab930511188dd0e807
jocker18291 <dawidans714@gmail.com>
2025-02-23 14:58:49 +0100

    Final stage, minor bug fixed

--- sumto99.cpp
+++ sumto99.cpp
@@ -5,11 +5,14 @@ using namespace std;
 int sum(){
     int n = 0;
     int S = 0;
-    while (n != 99){
+    while (true){
         cout << "Enter numbers to sum (only summing nums from -15 to 15; 99 will end the loop): ";
         cin >> n;
-        if (n <= -15 || n >= 15){
+        if (n >= -15 && n <= 15){
             S += n;
+        }
+        if(n == 99){
+            break;
         }
     }
     return S;
