193e8eadb0379e8ef01b38dad12370db72adfb50
jocker18291 <dawidans714@gmail.com>
2025-02-23 14:06:41 +0100

    Add linear equation support

--- quadratic.cpp
+++ quadratic.cpp
@@ -8,14 +8,21 @@ int main() {
     cout << "Enter your coefficients: ";
     cin >> a >> b >> c;
     float delta = (b * b) - 4 * a * c;
-    if (delta < 0) {
-        cout << "No real solutions." << endl;
+    if(a != 0){
+        if (delta < 0) {
+            cout << "No real solutions." << endl;
+        }
+        else if(delta == 0) {
+            cout << "The solution equals to: " << (-1 * b) / (2 * a) << endl;
+        }
+        else {
+            cout << "Solutions are equal to " << ((-1 * b) + sqrt(delta)) / (2 * a) << " and " << ((-1 * b) - sqrt(delta)) / (2 * a) << endl;
+        }
     }
-    else if(delta == 0) {
-        cout << "The solution equals to: " << (-1 * b) / (2 * a) << endl;
-    }
-    else {
-        cout << "Solutions are equal to " << ((-1 * b) + sqrt(delta)) / (2 * a) << " and " << ((-1 * b) - sqrt(delta)) / (2 * a) << endl;
+    else if(a == 0) {
+        if(b != 0){
+            cout << "It's linear and the solution is: " << (-1 * c) / b << endl;
+        }
     }
     return 0;
 }
