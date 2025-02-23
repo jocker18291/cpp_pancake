652dfa12994b21c6da64d6b52aefa3c73e72ef77
jocker18291 <dawidans714@gmail.com>
2025-02-23 23:52:33 +0100

    Add dynamic array and deleting it from memory

--- dynamicstr2.cpp
+++ dynamicstr2.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
 
 using namespace std;
 
-int main() {
+void dynArr(int arr[]) {
+    
+}
 
+int main() {
+    int *arr;
+    arr = new int[16 + rand() % (32 - 16 + 1)];
+    srand(time(0));
+    dynArr(arr);
+    delete []arr;
     return 0;
 }
