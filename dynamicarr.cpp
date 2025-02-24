f2f5c45361272ce299e17a7e0acfee60f0d76734
jocker18291 <dawidans714@gmail.com>
2025-02-24 12:58:27 +0100

    Add resize function

--- dynamicarr.cpp
+++ dynamicarr.cpp
@@ -8,6 +8,16 @@ private:
     int capacity;
     int *arr;
 
+    void resize(int *arr) {
+        capacity *= 2;
+        int *newArr = new int[capacity];
+        for(int i = 0; i < size; i++) {
+            newArr[i] = arr[i];
+        }
+        delete []arr;
+        arr = newArr;
+    }
+
 public:
     DynamicArray() {
         capacity = 4;
