2a98fbdc9e5125a75da0fcd3b6b1dbb7847c22f5
jocker18291 <dawidans714@gmail.com>
2025-02-24 10:38:20 +0100

    Add convertion of capital chars to small chars and a map

---
+++ histogram.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+void histogram(string str) {
+    map<char, int> count;
+    for(int i = 0; i < str.length(); i++) {
+        if(str[i] >= 65 && str[i] <= 90) {
+            str[i] += 32;
+        }
+    }
+}
+
+int main() {
+    string str;
+    cout << "Enter a text: ";
+    cin >> str;
+    return 0;
+}
