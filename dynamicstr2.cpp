29ac338978fcf3656fa8321bbd4fcd0bd35b41b3
jocker18291 <dawidans714@gmail.com>
2025-02-24 10:11:42 +0100

    Add random range function with display

--- dynamicstr2.cpp
+++ dynamicstr2.cpp
@@ -5,15 +5,25 @@
 
 using namespace std;
 
-void dynArr(int arr[]) {
-    
+void dynArr(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = pow(2.0, i);
+    }
+}
+
+void display(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << "[" << i << "] 2^" << i << " = " << arr[i] << endl;
+    }
 }
 
 int main() {
-    int *arr;
-    arr = new int[16 + rand() % (32 - 16 + 1)];
     srand(time(0));
-    dynArr(arr);
+    int *arr;
+    int size = 16 + rand() % (32 - 16 + 1);
+    arr = new int[size];
+    dynArr(arr, size);
+    display(arr, size);
     delete []arr;
     return 0;
 }
