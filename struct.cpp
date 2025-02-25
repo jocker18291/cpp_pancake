c2569643396512fa7d7526548b2cd540f68dddb6
jocker18291 <dawidans714@gmail.com>
2025-02-25 13:35:17 +0100

    Add present structure. Looking forward to add methods

---
+++ struct.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+struct Present {
+private:
+    string code;
+    string content;
+    float weight;
+    char size;
+    string color;
+};
+
+int main() {
+
+    return 0;
+}
